d2be7e533914f52a371ef2c5b4b0e30ddb7cc548
Syd Logan <slogan621@gmail.com>
2016-07-01 15:40:17 -0700

    change auth settings for REST framework

--- tscharts/settings.py
+++ tscharts/settings.py
@@ -137,6 +137,6 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ]
 }
