a34c9e6f881764741cc8db71953cf1b8cb512f94
Syd Logan <slogan621@gmail.com>
2017-03-06 23:13:07 -0800

    define models and API for routingslip

--- routingslip/README.txt
+++ routingslip/README.txt
@@ -1,18 +1,60 @@
 Routing slip service
 
+Routing slip is a list of station IDs associated with a clinic and patient.
+
 routingslip
    get 
-/tscharts/routinglslip/id
-   returns routing slip with given ID
+/tscharts/routingslip
+   {clinic_id:id}
+   returns array of routing slip ids
+/tscharts/routingslip/id
+   returns routing slip with given ID (see blow)
 /tscharts/routingslip
    {patient_id:id, clinic_id:id}
-   above parameters filter if present
-   returns array of routing slip ids
+   both clinic_id and patient_id are required
+
+The last two of the above return data as following:
+   { 
+     -- routing slip id
+     id: id
+     -- how patient was categorized at registration (or later)
+     category: category,   
+     -- array of routing slip entries in order of visitation
+     routing: [                  
+                 {
+                  station: id,
+                  state: state
+                 },
+                 ...
+              ],
+
+      -- array of comments in timestamp order
+      comments: [ 
+                    {
+                     author: id,
+                     timestamp: datetime,
+                     comment: comment_text
+                    },
+                    ...
+                ]
+    }
    post
 /tscharts/routingslip 
-   {patient_id:id, clinic_id:id, ....}
+   {
+    patient_id: id, -- required
+    clinic_id: id,  -- required
+    category: categoryid,  -- optional, but typically supplied
+    routing: [ordered array of clinicstation ids] -- optional
+   }
    returns routing slip id 
    put
 /tscharts/routingslip/id
+   -- at least one of the fields in below payload must be present or param error
+   {
+    category: category,    -- change category of patient
+    routing: [ordered array of clinicstation ids],  -- change routing order
+    state: {clinicstation:id, state:state} -- change state of an entry
+    comment: {author:id, comment:text} -- add a comment
+   }
    delete
 /tscharts/routingslip/id
--- routingslip/models.py
+++ routingslip/models.py
@@ -1,5 +1,73 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from clinic.models import Clinic
+from patient.models import Patient
+from clinicstation.models import ClinicStation 
+
+'''
+Routing slip identifies the patient, the clinic, and category of patient.
+The category may change from clinic to clinic based on care given.
+'''
+
+class RoutingSlip(models.Model):
+    patient = models.ForeignKey(Patient)
+    clinic = models.ForeignKey(Clinic)
+    NEWCLEFT = 'n'
+    DENTAL = 'd'
+    RETCLEFT = 'r'
+    ORTHO = 'o'
+    OTHER = 't'
+    UNKNOWN = 'u'
+    CATEGORY_CHOICES = ((NEWCLEFT, "New Cleft"), 
+                        (DENTAL, "Dental"),
+                        (RETCLEFT, "Returning Cleft"),
+                        (ORTHO, "Ortho"),
+                        (OTHER, "Other"),
+                        (UNKNOWN, "Unknown"),)
+
+    category = models.CharField(
+        max_length = 1,
+        choices = CATEGORY_CHOICES,
+        default = OTHER,
+    )
+
+'''
+A given patient will be associated with a routing slip and a set of 
+stations that he or she must visit. A record in this table defines
+one such station and its order of visitation. Its order may change
+during the clinic. Routing slip entries can be added or removed to
+the routing slip during the patient stay. The initial set is defined
+by the patient classification (e.g., dental patients only need to be
+routed to dental). A specialist can add, remove, or change the 
+relative order of items in the routing slip. When an item is added
+to the routing slip, its state is SCHEDULED. Removal does not result
+in the record being deleted from the database, it is just a state 
+change REMOVED. Once a patient has visited a station, the routing 
+slip entry is marked VISITED.
+'''
+ 
+class RoutingSlipEntry(models.Model):
+    routingslip = models.ForeignKey(RoutingSlip)
+    clinicstation = models.ForeignKey(ClinicStation)
+    order = models.IntegerField(default=0)   
+    SCHEDULED = 'a'   # checkin to station is pending (or clinic ended)
+    VISITED = 'v'     # patient was checked in at the station
+    REMOVED = 'r'     # entry was removed by someone before seen by station
+    STATE_CHOICES = ((SCHEDULED, "Scheduled"), 
+                     (VISITED, "Visited"),
+                     (REMOVED, "Removed"),)
+
+    state = models.CharField(
+        max_length = 1,
+        choices = STATE_CHOICES,
+        default = SCHEDULED,
+    )
+
+class RoutingSlipComment(models.Model):
+    routingslip = models.ForeignKey(RoutingSlip)
+    comment = models.TextField()
+    author = models.ForeignKey(User)    # logged in user that added the comment
+    updatetime = models.DateTimeField(auto_now=True)
