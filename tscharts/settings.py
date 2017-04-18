fdbe23a8cf4766c49f136700cef8bfb986a7c52e
Syd Logan <slogan621@gmail.com>
2017-04-18 01:14:07 -0700

    add image application

--- tscharts/settings.py
+++ tscharts/settings.py
@@ -30,6 +30,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+CHART_IMAGES_DIR = "/opt/thousandsmiles/images"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'clinic.apps.ClinicConfig',
     'clinicstation.apps.ClinicstationConfig',
+    'image',
     'medicalhistory',
     'patient',
     'register',
