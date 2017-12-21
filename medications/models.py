2364787a289a4f274b68f1886f7d545a77341764
Xinyue Han <xinyueh7@uci.edu>
2017-12-21 18:59:18 +0000

    modify models.py

--- medications/models.py
+++ medications/models.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+class Medications(models.Model):
+    medication = models.CharField(max_length = 300)
 
-# Create your models here.
+
