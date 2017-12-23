1f7a72a3bc49c126f0b6feb90dd4241b1381586d
Xinyue Han <xinyueh7@uci.edu>
2017-12-23 21:50:25 +0000

    change medication to name

--- medications/models.py
+++ medications/models.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 class Medications(models.Model):
-    medication = models.CharField(max_length = 300)
+    name = models.CharField(max_length = 300)
 
 
