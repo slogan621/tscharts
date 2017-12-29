2d55614475729428eb84821c356b3177673d8418
Xinyue Han <xinyueh7@uci.edu>
2017-12-29 21:04:11 +0000

    Import CreateMedications and GetMedications from test file. Change the name of class and instances. Allow usersto provide FDA file name when they log in.

--- tools/fdainsert/fdainsert.py
+++ tools/fdainsert/fdainsert.py
@@ -3,78 +3,48 @@ import json
 
 from service.serviceapi import ServiceAPI
 from test.tscharts.tscharts import Login, Logout
+from test.medications.medications import CreateMedications, GetMedications
 
-def drug_set(file_name):
-    ret = set()
-    file_opened = open(file_name,"r")
-    for line in file_opened:
-        drug = (line.split('\t'))[5].upper().rstrip()
-        ret.add(drug)
-    file_opened.close()
-    return ret
-
-class CreateMedications(ServiceAPI):
-    def __init__(self, host, port, token, payload):
-        super(CreateMedications, self).__init__()
-
-        self.setHttpMethod("POST")
-        self.setHost(host)
-        self.setPort(port)
-        self.setToken(token)
-
-        self.setPayload(payload)
-        self.setURL("tscharts/v1/medications/")
-
-class GetMedications(ServiceAPI):
-    def __init__(self, host, port, token):
-        super(GetMedications, self).__init__()
-      
-        self.setHttpMethod("GET")
-        self.setHost(host)
-        self.setPort(port)
-        self.setToken(token)
-        self.setURL("tscharts/v1/medications/")
-
-class DeleteMedications(ServiceAPI):
-    def __init__(self, host, port, token, id):
-        super(DeleteMedications, self).__init__()
-        self.setHttpMethod("DELETE")
-        self.setHost(host)
-        self.setPort(port)
-        self.setToken(token)
-        self.setURL("tscharts/v1/medications/{}/".format(id))
-
-class AcquireMedicationsList():
+class UpdateMedicationsList():
     def __init__(self):
         login = Login(host, port, username, password)
         ret = login.send(timeout=30)
         global token
         token = ret[1]["token"]
+
+    def readDrugFromFile(self, filename):
+        ret = set()
+        file = open(filename, "r")
+        for x in file:
+            drug = (x.split('\t'))[5].upper().rstrip()
+            ret.add(drug)
+        file.close()
+        return ret      
         
     def createMedications(self):
-        aset = drug_set("Products.txt")
-        new_drug = []
-        for medication in aset:
+        druginfile = self.readDrugFromFile(filename)
+        newdrug = []
+        for medication in druginfile:
             data = {}
             data["name"] = medication
             x = CreateMedications(host, port, token, data)
             ret = x.send(timeout = 30)
             if ret[0] == 200:
-                new_drug.append(medication)
-        return new_drug
+                newdrug.append(medication)
+        return newdrug
        
     def getMedications(self):
         x = GetMedications(host, port, token)
         ret = x.send(timeout = 30)
         ret = ret[1]
         return ret
-    
+
 def usage():
-    print("medications [-h host] [-p port] [-u username] [-w password]") 
+    print("medications [-h host] [-p port] [-u username] [-w password] [-f filename]") 
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h:p:u:w:")
+        opts, args = getopt.getopt(sys.argv[1:], "h:p:u:w:f:")
     except getopt.GetoptError as err:
         print str(err) 
         usage()
@@ -87,6 +57,8 @@ def main():
     username = None
     global password
     password = None
+    global filename
+    filename = None
     for o, a in opts:
         if o == "-h":
             host = a
@@ -96,20 +68,24 @@ def main():
             username = a
         elif o == "-w":
             password = a
+        elif o == "-f":
+            filename = a
         else:   
             assert False, "unhandled option"
 
-    x = AcquireMedicationsList()
-    new_drug =  x.createMedications()
-    drug_list = x.getMedications()
+    x = UpdateMedicationsList()
+    druginfile = x.readDrugFromFile(filename)
+    print("Current fda file({}) contains {} drugs.".format(filename, len(druginfile)))
+    print("Updating drug list...")
+    newdrug =  x.createMedications()
+    druglist = x.getMedications()
 
-    if len(new_drug) == 0:
+    if len(newdrug) == 0:
         print("No added new drugs")
     else:
-        print("There are {} added new drugs:{}.".format(len(new_drug), ",".join(str(x) for x in new_drug)))
+        print("There are {} added new drugs:{}.".format(len(newdrug), ",".join(str(x) for x in newdrug)))
 
-    print("Current drug list contains {} drugs.".format(len(drug_list)))
+    print("Current drug list contains {} drugs.".format(len(druglist)))
 
 if __name__ == "__main__":
     main()
-
