44ac17a6d92e47587e0e916cf7e413b17990d7f5
Xinyue Han <xinyueh7@uci.edu>
2018-06-21 05:39:17 +0000

    add consent app

--- consent/models.py
+++ consent/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from register.models import Register
+
+class Consent(models.Model):
+    register = models.ForeignKey(Register)
+    general_consent = models.BooleanField(default = False)
+    photo_consent = models.BooleanField(default = False)
--- tscharts/settings.py
+++ tscharts/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'surgerytype',
     'category',
     'surgeryhistory',
+    'consent',
 ]
 
 MIDDLEWARE_CLASSES = [
