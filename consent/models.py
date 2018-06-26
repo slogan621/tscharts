45b108393f38bf269ce5d7b9cf1476045dd30481
Xinyue Han <xinyueh7@uci.edu>
2018-06-26 05:28:49 +0000

    add patient and clinic as foreign keys

--- consent/models.py
+++ consent/models.py
@@ -4,8 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 from register.models import Register
+from patient.models import Patient
+from clinic.models import Clinic
 
 class Consent(models.Model):
-    register = models.ForeignKey(Register)
+    registration = models.ForeignKey(Register)
+    patient = models.ForeignKey(Patient)
+    clinic = models.ForeignKey(Clinic)
     general_consent = models.BooleanField(default = False)
     photo_consent = models.BooleanField(default = False)
