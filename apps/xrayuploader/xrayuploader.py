93cf7fb2e80daf18a0dc8f7dabb045379e605bdd
Syd Logan <slogan621@gmail.com>
2020-09-05 12:20:12 -0700

    clinic list control scaffolding

--- apps/xrayuploader/xrayuploader.py
+++ apps/xrayuploader/xrayuploader.py
@@ -1,12 +1,19 @@
 import getopt, sys
 import json
 import time
+import wx
+
+from advanced_search import AdvancedSearch
+from regular_search import RegularSearch
+from pubsub import pub
 
 from service.serviceapi import ServiceAPI
 from test.tscharts.tscharts import Login, Logout
 from test.register.register import GetAllRegistrations
 from test.patient.patient import GetPatient
 from test.clinic.clinic import GetAllClinics
+from test.image.image import GetImage
+from collections import OrderedDict
 
 class TSSession():
     def __init__(self):
@@ -63,7 +70,11 @@ class Clinics():
         x = GetAllClinics(sess.getHost(), sess.getPort(), sess.getToken())
         ret = x.send(timeout=30)
         if ret[0] == 200:
-            ret = ret[1]
+            tmp = []
+            for x in ret[1]:
+                #tmp.append((x["id"], "Clinic Number: {} Location: {} Start: {} End: {}".format(x["id"], x["location"], x["start"], x["end"])))
+                tmp.append(x)
+            ret = sorted(tmp, key = lambda i: i['start'], reverse=True) 
         else:
             ret = None
         return ret
@@ -90,6 +101,7 @@ class Registrations():
                 y = self.getPatient(sess, x["patient"])
                 if y:
                     p = {}
+                    p["id"] = y["id"]
                     p["first"] = y["first"]
                     p["middle"] = y["middle"]
                     p["paternal_last"] = y["paternal_last"]
@@ -101,6 +113,112 @@ class Registrations():
 
 def usage():
     print("xrayuploader [-h host] [-p port] -u username -w password") 
+
+class MainPanel(wx.Panel):
+
+    def __init__(self, parent, sess, clinics):
+        super().__init__(parent)
+        pub.subscribe(self.update_ui, 'update_ui')
+
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        search_sizer = wx.BoxSizer()
+        clinics_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        txt = 'Select Clinic and Patient'
+        label = wx.StaticText(self, label=txt)
+        self.main_sizer.Add(label, 0, wx.ALL, 5)
+        self.clinicIds = []
+        for x in clinics:
+            self.clinicIds.append(x["id"])
+                #tmp.append((x["id"], "Clinic Number: {} Location: {} Start: {} End: {}".format(x["id"], x["location"], x["start"], x["end"])))
+        self.clinics = wx.ListCtrl(self, size=(-1, -1), style=wx.LC_REPORT)
+        
+        #tmp.append((x["id"], "Clinic Number: {} Location: {} Start: {} End: {}".format(x["id"], x["location"], x["start"], x["end"])))
+        self.clinics.InsertColumn(0, "Clinic Number")
+        self.clinics.InsertColumn(1, "Location")
+        self.clinics.InsertColumn(2, "Start")
+        self.clinics.InsertColumn(3, "End")
+
+        index = 0
+        for i in clinics: 
+            self.clinics.InsertStringItem(index, str(i["id"])) 
+            self.clinics.SetStringItem(index, 1, i["location"]) 
+            self.clinics.SetStringItem(index, 2, i["start"])
+            self.clinics.SetStringItem(index, 3, i["end"])
+            index += 1
+
+        self.clinics.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_clinic, self.clinics)
+        clinics_sizer.Add(self.clinics, 1, wx.EXPAND)
+        self.search = wx.SearchCtrl(
+            self, style=wx.TE_PROCESS_ENTER, size=(-1, 25))
+        self.search.Bind(wx.EVT_SEARCHCTRL_SEARCH_BTN, self.on_search)
+        self.search.Bind(wx.EVT_TEXT_ENTER, self.on_search)
+        search_sizer.Add(self.search, 1, wx.EXPAND)
+
+        '''
+        self.advanced_search_btn = wx.Button(self, label='Advanced Search',
+                                    size=(-1, 25))
+        self.advanced_search_btn.Bind(wx.EVT_BUTTON, self.on_advanced_search)
+        search_sizer.Add(self.advanced_search_btn, 0, wx.ALL, 5)
+        '''
+
+        self.main_sizer.Add(clinics_sizer, 1, wx.ALL|wx.EXPAND, 5)
+        self.main_sizer.Add(search_sizer, 0, wx.EXPAND)
+
+        self.search_panel = RegularSearch(self, sess)
+        self.advanced_search_panel = AdvancedSearch(self)
+        self.advanced_search_panel.Hide()
+        self.main_sizer.Add(self.search_panel, 1, wx.EXPAND)
+        self.main_sizer.Add(self.advanced_search_panel, 1, wx.EXPAND)
+
+        self.SetSizer(self.main_sizer)
+
+    def set_registrations(self, registrations):
+        self.search_panel.load_search_results(registrations)
+
+    def on_clinic(self, event):
+        ind = event.GetIndex()
+        item = self.clinics.GetItem(ind, 0)
+        print("{}".format(item.GetText()))
+
+    def on_search(self, event):
+        print("on search enter")
+        search_results = []
+        search_term = event.GetString()
+        if search_term:
+            query = {'q': search_term, 'media_type': 'image'}
+            pub.sendMessage('search_results', query=query)
+
+    def on_advanced_search(self, event):
+        self.search.Hide()
+        self.search_panel.Hide()
+        self.advanced_search_btn.Hide()
+        self.advanced_search_panel.Show()
+        self.main_sizer.Layout()
+
+    def update_ui(self):
+        """
+        Hide advanced search and re-show original screen
+
+        Called by pubsub when advanced search is invoked
+        """
+        self.advanced_search_panel.Hide()
+        self.search.Show()
+        self.search_panel.Show()
+        self.advanced_search_btn.Show()
+        self.main_sizer.Layout()
+
+class SearchFrame(wx.Frame):
+
+    def __init__(self, sess, clinics):
+        super().__init__(None, title='Select Patient',
+                         size=(1200, 800))
+        panel = MainPanel(self, sess, clinics)
+        self.Show()
+        regs = Registrations()
+        print("{}".format(clinics))
+        patientsThisClinic = regs.getAllRegistrations(sess, clinics[0]["id"])
+        panel.set_registrations(patientsThisClinic)
 
 def main():
     try:
@@ -136,11 +254,18 @@ def main():
     clinics = c.getAllClinics(session)
     for x in clinics:
         print("{}".format(x))   
-    
-    clinicid = 1
+   
+    clinicid = int(clinics[0]["id"])
+
+    '''
     patientsThisClinic = regs.getAllRegistrations(session, clinicid)
     for x in patientsThisClinic:
         print("{}".format(x))   
+    '''
+
+    app = wx.App(False)
+    frame = SearchFrame(session, clinics)
+    app.MainLoop()
 
 if __name__ == "__main__":
     main()
