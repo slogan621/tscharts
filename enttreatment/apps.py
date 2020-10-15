28f744249b3a718f3092d3b3365160f4397a326d
Syd Logan <slogan621@gmail.com>
2020-10-15 08:43:56 -0700

    rename ENTPlanConfig

--- enttreatment/apps.py
+++ enttreatment/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class ENTPlanConfig(AppConfig):
-    name = 'entplan'
+class ENTTreatmentConfig(AppConfig):
+    name = 'enttreatment'
