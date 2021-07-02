e826f2a0f8c98dd1ebffd4b5ea79f666c39cad0f
Syd Logan <slogan621@gmail.com>
2021-07-01 20:31:18 -0700

    changes made for most recent clinic

--- tscharts/wsgi.py
+++ tscharts/wsgi.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-sys.path.append('/home/slogan')
-sys.path.append('/home/slogan/')
-sys.path.append('/home/slogan/ts/tscharts')
-sys.path.append('/home/slogan/ts/tscharts/')
-sys.path.append('/home/slogan/ts/tscharts/tscharts')
-sys.path.append('/home/slogan/ts/tscharts/tscharts/')
+sys.path.append('/home/tscharts')
+sys.path.append('/home/tscharts/')
+sys.path.append('/home/tscharts/ts/tscharts')
+sys.path.append('/home/tscharts/ts/tscharts/')
+sys.path.append('/home/tscharts/ts/tscharts/tscharts')
+sys.path.append('/home/tscharts/ts/tscharts/tscharts/')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
