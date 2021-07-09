56f7aa1e7b51d584f8b4dce5d52a9d433feb6b21
Dale Proctor <dfproctor@gmail.com>
2021-07-08 19:46:39 -0700

    added Syd's changes

--- apps/tsdashboard/wristbandprinter.py
+++ apps/tsdashboard/wristbandprinter.py
@@ -13,29 +13,6 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-
-# Notes:
-# Printing from the raspberry pi uses the CUPS driver solution.
-# General Info on the Zebra ZD510-HC can be found at:
-# https://www.zebra.com/us/en/support-downloads/printers/desktop/zd510-hc.html
-# There is a specific link for information on the CUPS package.
-#
-# Once installed, the driver needs to be configured for this printer:
-# Size: custom
-# Width = 1
-# Heigth = 7.024 (May need to change for different sizes.
-# Units: Inches
-# Resolution: 300dpi
-# Meadia Tracking: Non-continuous (Mark sensing)
-# Mesia Type: Printer default
-#
-# In my .bashrc file, I also set this:
-# export PRINTER="Zebra_Technologies_ZTC_ZD510-300dpi_ZPL"
-#
-# The application uses LPR, so this export is required.
-
-
-
 class WristBandPrinter:
     def __init__(self):
         pass
@@ -43,25 +20,22 @@ class WristBandPrinter:
     def getUserFormattedWristband(self):
         return self.__str__();
 
-# Because the second line cut off the first few characters,
-# place six spaces after the "\n" on the beginning of the second line.
-
     def __str__(self):
         val = "{}{} {}{} {}{} {}{} {}{} {}{} {}{} {}".format(
         str(self._patientData.id) if self._idShow else "",
-        "\n      " if self._idnl else "",
+        "\n" if self._idnl else "",
         self._patientData.first if self._firstShow else "",
-        "\n      " if self._firstnl else "",
+        "\n" if self._firstnl else "",
         self._patientData.middle if self._middleShow else "",
-        "\n      " if self._middlenl else "",
+        "\n" if self._middlenl else "",
         self._patientData.father.upper() if self._fatherShow else "",
-        "\n      " if self._fathernl else "",
+        "\n" if self._fathernl else "",
         self._patientData.mother if self._motherShow else "",
-        "\n      " if self._mothernl else "",
+        "\n" if self._mothernl else "",
         self._patientData.gender if self._genderShow else "",
-        "\n      " if self._gendernl else "",
+        "\n" if self._gendernl else "",
         self._patientData.dob if self._dobShow else "",
-        "\n      " if self._dobnl else "",
+        "\n" if self._dobnl else "",
         self._patientData.curp if self._curpShow else "")
 
         return val
@@ -91,40 +65,80 @@ curp):
         self._genderShow = gender
         self._curpShow = curp
 
+    def sendToPrinter(self):
+        pass
+
+# Notes:
+# Printing from the raspberry pi uses the CUPS driver solution.
+# General Info on the Zebra ZD510-HC can be found at:
+# https://www.zebra.com/us/en/support-downloads/printers/desktop/zd510-hc.html
+# There is a specific link for information on the CUPS package.
 #
-# This procedure actually sends the ASCII data to the printer using lpr.
-# First open a temporary file in /tmp
-# Then send a newline and six spaces to the file.
-# Without the spaces, the patient ID is cut off.
-# Then send the wristband string to the file
-# Close the file.
-# The original print to the screen is still present.
-# Then, using the command lpr, print the file to the zebra printer
+# Once installed, the driver needs to be configured for this printer:
+# Size: custom
+# Width = 1
+# Heigth = 7.024 (May need to change for different sizes.
+# Units: Inches
+# Resolution: 300dpi
+# Meadia Tracking: Non-continuous (Mark sensing)
+# Mesia Type: Printer default
 #
-# Asumption: The same filename is used each time. The asumption is that lpr will
-# read in the file before another one is created. The printing is very fast, so
-# hopefully this will work. Otherwise, a temporary filename will be needed, and
-# cleanup will have to be done. This way no cleanup is needed.
+# In my .bashrc file, I also set this:
+# export PRINTER="Zebra_Technologies_ZTC_ZD510-300dpi_ZPL"
 #
+# The application uses LPR, so this export is required.
+
+class LPRTextWristBandPrinter(WristBandPrinter):
+    
+    def __init__(self):
+        super().__init__()
+
+    # Because the second line cut off the first few characters,
+    # place six spaces after the "\n" on the beginning of each
+    # line following newlines that the user embedded.
+
+    def __str__(self):
+
+        val = super().__str__()
+        val = val.replace("\n", "\n      ")
+        return val
+    #
+    # This procedure actually sends the ASCII data to the printer using lpr.
+    # First open a temporary file in /tmp
+    # Then send a newline and six spaces to the file.
+    # Without the spaces, the patient ID is cut off.
+    # Then send the wristband string to the file
+    # Close the file.
+    # The original print to the screen is still present.
+    # Then, using the command lpr, print the file to the zebra printer
+
     def sendToPrinter(self):
         import sys
         import os
+        import tempfile
 
         wristbandStr = self.getUserFormattedWristband()
-        
-        f= open("/tmp/test.txt","w+")
+     
+        # generate a temp file. lpr is a spooler, and if
+        # a second print job comes in before lpr has queued
+        # the file with same name, will overwrite.
+ 
+        path = tempfile.mkstemp(dir="/tmp", text=True)[1] 
+        f = open(path, "w+")
         f.write("\n      ")
         f.write(wristbandStr)
         f.close()
         print("{}".format(wristbandStr))
-        os.system("lpr -o orientation-requested=4 /tmp/test.txt")
 
+        # -r tells lpr to remove the file after spooling
+
+        os.system("lpr -r -o orientation-requested=4 {}".format(path))
 
 def main():
 
     # example code as called by UI
 
-    printer = WristBandPrinter()
+    printer = LPRTextWristBandPrinter()
 
     patientData = lambda: None
     patientData.id = 1234
