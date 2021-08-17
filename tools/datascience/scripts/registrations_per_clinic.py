97753d30a87c2ae5cc69c0128b5f90cc7c028b35
Syd Logan <slogan621@gmail.com>
2021-08-16 17:38:25 -0700

    plot registrations per clinic pandas

---
+++ tools/datascience/scripts/registrations_per_clinic.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('~/thousandsmiles/analysis/08-2021/tscharts-output/register_register-final.txt', encoding="latin-1", sep="__")
+
+# drop the first 11 rows, these were earlier clinics with restarts and
+# not worth displaying
+
+N = 11
+df = df.iloc[N: , :]
+
+clinicdf = pd.read_csv('~/thousandsmiles/analysis/08-2021/tscharts-output/clinic_clinic-final.txt', encoding="latin-1", sep="__")
+
+clinicdf = clinicdf.iloc[N: , :]
+
+df['clinic_id'] = df['clinic_id'].map(clinicdf.set_index('id')['start'])
+
+df['clinic_id'].value_counts().sort_index().plot(kind='bar')
+plt.title("Registrations per Clinic")
+plt.xlabel("Clinic ID")
+plt.ylabel("Count")
+plt.show()
+
