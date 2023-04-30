0b2ece44a5aa733a9a3e6e4a1e4c12888bc9fbeb
Syd Logan <slogan621@gmail.com>
2023-04-29 20:20:40 -0700

    age in years plots with pandas

---
+++ tools/datascience/scripts/age_in_years.py
@@ -0,0 +1,65 @@
+#(C) Copyright Syd Logan 2023
+#(C) Copyright Thousand Smiles Foundation 2023
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#
+#You may obtain a copy of the License at
+#http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+def compute_age(row):
+    d1 = datetime.strptime(row["start"], "%Y-%m-%d")
+    d2 = datetime.strptime(row["dob"], "%Y-%m-%d")
+    diff_in_years = relativedelta(d1, d2)
+    return diff_in_years.years
+
+# drop the first 11 rows, these were earlier clinics with restarts and
+# not worth displaying
+
+N = 11
+
+clinicdf = pd.read_csv('tscharts-output/clinic_clinic-final.txt', encoding="latin-1", sep="__")
+clinicdf = clinicdf.iloc[N: , :]
+
+registerdf = pd.read_csv('tscharts-output/register_register-final.txt', encoding="latin-1", sep="__")
+
+
+merged_df = pd.merge(registerdf, clinicdf, left_on="clinic_id", right_on="id")
+
+#df['clinic_id'] = df['clinic_id'].map(clinicdf.set_index('id')['start'])
+
+patientdf = pd.read_csv('tscharts-output/patient_massaged-final.txt', encoding="latin-1", sep="__")
+
+merged_df = pd.merge(patientdf, merged_df, left_on="id", right_on="patient_id")
+
+# use a lambda to create a new row, which is age in years of patient
+
+merged_df["age_in_years"] = merged_df.apply(lambda row: compute_age(row), axis=1)
+
+print(merged_df)
+
+#di = {'m': "Male", 'f': "Female"}
+#df = df.replace({"gender": di})
+merged_df['age_in_years'].value_counts().plot(kind='bar', alpha=0.75, rot=0)
+plt.title("Number of Patients By Age")
+plt.xlabel("Age")
+plt.ylabel("Count")
+plt.show()
+
+merged_df['age_in_years'].value_counts().sort_index().plot(kind='bar', alpha=0.75, rot=0)
+plt.title("Number of Patients By Age")
+plt.xlabel("Age")
+plt.ylabel("Count")
+plt.show()
+
