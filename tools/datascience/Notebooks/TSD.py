ee97c975d38b2c3e23e4ab6154142babe31cb878
Will <ww994a@gmail.com>
2023-05-10 22:36:21 +0000

    Changed variable names in TSD
    updated TSD so that prefix df_ can be used to show all data frames.

--- tools/datascience/Notebooks/TSD.py
+++ tools/datascience/Notebooks/TSD.py
@@ -1,3 +1,21 @@
+#(C) Copyright Thousand Smiles Foundation 2023
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#
+#You may obtain a copy of the License at
+#http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+
+#DataFrames begin with prefix "df_" and can be found easily with Auto Complete
+#FileLocations bigeing with prefix "file_loc_"
+
 import pandas as pd
 
 #from os import listdir
@@ -8,44 +26,44 @@ import pandas as pd
 
 #centralized location for file paths
 file_prefix = '../../../../../1000_Smiles/Data/tscharts-output/'
-file_loc = file_prefix+'clinic_clinic-final.txt'
-file_loc2 = file_prefix+'register_register-final.txt'
-file_loc3 = file_prefix+'patient_massaged-final.txt'
-file_loc4 = file_prefix+'image_image-final.txt'
-file_loc5 = file_prefix+'medicalhistory_medicalhistory-final.txt'
-file_loc6 = file_prefix+'routingslip_routingslip-final.txt'
+file_loc_clinic = file_prefix+'clinic_clinic-final.txt'
+file_loc_register = file_prefix+'register_register-final.txt'
+file_loc_patient = file_prefix+'patient_massaged-final.txt'
+file_loc_images = file_prefix+'image_image-final.txt'
+file_loc_history = file_prefix+'medicalhistory_medicalhistory-final.txt'
+file_loc_routing = file_prefix+'routingslip_routingslip-final.txt'
 
-clinic_df = pd.read_csv(file_loc, encoding="latin-1", sep="__", engine ='python')
+df_clinic = pd.read_csv(file_loc_clinic, encoding="latin-1", sep="__", engine ='python')
 N = 11 
 # drop the first 11 rows, these were earlier clinics with restarts and
 # not worth displaying
-clinic_df = clinic_df.iloc[N: , :]
+df_clinic = df_clinic.iloc[N: , :]
 
 #merged dataframes - patients
-register_df = pd.read_csv(file_loc2, encoding="latin-1", sep="__", engine ='python')
-merged_df = pd.merge(register_df, clinic_df, left_on="clinic_id", right_on="id")
-patient_df = pd.read_csv(file_loc3, encoding="latin-1", sep="__", engine="python")
-merged_df = pd.merge(patient_df, merged_df, left_on="id", right_on="patient_id")
+df_register = pd.read_csv(file_loc_register, encoding="latin-1", sep="__", engine ='python')
+df_merged = pd.merge(df_register, df_clinic, left_on="clinic_id", right_on="id")
+df_patient = pd.read_csv(file_loc_patient, encoding="latin-1", sep="__", engine="python")
+df_merged = pd.merge(df_patient, df_merged, left_on="id", right_on="patient_id")
 
 
 #clinic dataframes
-images_df = pd.read_csv(file_loc4, encoding="latin-1", sep="__", engine="python")
-headshot_df = images_df.copy(deep=True)
-headshot_df = headshot_df[headshot_df['imagetype'] == 'h']
-headshot_df['clinic_id'] = headshot_df['clinic_id'].map(clinic_df.set_index('id')['start'])
+df_images = pd.read_csv(file_loc_images, encoding="latin-1", sep="__", engine="python")
+df_headshot = df_images.copy(deep=True)
+df_headshot = df_headshot[df_headshot['imagetype'] == 'h']
+df_headshot['clinic_id'] = df_headshot['clinic_id'].map(df_clinic.set_index('id')['start'])
 
-xray_df = images_df.copy(deep=True)
-xray_df = xray_df[xray_df['imagetype'] == 'x']
-xray_df['clinic_id'] = xray_df['clinic_id'].map(clinic_df.set_index('id')['start'])
-
-
+df_xray = df_images.copy(deep=True)
+df_xray = df_xray[df_xray['imagetype'] == 'x']
+df_xray['clinic_id'] = df_xray['clinic_id'].map(df_clinic.set_index('id')['start'])
 
 
-routing_df = pd.read_csv(file_loc6, encoding="latin-1", sep="__", engine="python")
+
+
+df_routing = pd.read_csv(file_loc_routing, encoding="latin-1", sep="__", engine="python")
 di = {'d': "Dental", 'r': "Returning Cleft", 'n': "New Cleft", 'o': "Ortho", 't': "Other", 'u': "Unknown", 'h': "Hearing Aids", 'e': "Ears"}
-routing_df = routing_df.replace({"category": di})
-routing_df['clinic_id'] = routing_df['clinic_id'].map(clinic_df.set_index('id')['start'])
+df_routing = df_routing.replace({"category": di})
+df_routing['clinic_id'] = df_routing['clinic_id'].map(df_clinic.set_index('id')['start'])
 
 
 #medical history
-medical_df = pd.read_csv(file_loc5, encoding="latin-1", sep="__", engine="python")
+df_medical = pd.read_csv(file_loc_history, encoding="latin-1", sep="__", engine="python")
